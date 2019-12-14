a1ad2d40d86a77f50433882524abed3b9c5d45dc
Kevin Lai <kevinlai76@gmail.com>
2019-12-14 01:50:25 -0500

    changed event listener to submit

--- main.js
+++ main.js
@@ -3,11 +3,7 @@ let form = document.querySelector('form')
 let input = document.querySelector('input')
 
 form.addEventListener('submit', (event)=>{
-    if(event.target.type === 'submit'){
         event.preventDefault()
         debugger;
-    } else if (event.target.type === 'text') {
-        console.log('you clicked on the input')
-    }
 })
 
