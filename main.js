98c665a143b19f982f65c6b0ba797ae32ccc2885
Kevin Lai <kevinlai76@gmail.com>
2019-12-16 14:22:18 -0500

    create li function

--- main.js
+++ main.js
@@ -2,8 +2,20 @@ let body = document.querySelector('body')
 let form = document.querySelector('form')
 let input = document.querySelector('input')
 
-form.addEventListener('submit', (event)=>{
+form.addEventListener('submit', (event) => {
         event.preventDefault()
-        debugger;
+        createLi(event)
+        event.target.firstElementChild.value = ''
 })
+
+function createLi(event){
+        let ul = document.querySelector('.content')
+        let li = document.createElement('li')
+        let inputValue = event.target.firstElementChild.value
+        li.innerHTML= `
+                <p>${inputValue}<p>
+        `
+        ul.append(li)
+        console.log(ul)
+}
 
