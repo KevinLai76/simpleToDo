c32c0af81edecd5a79e55caddd56ddb8a9bef38c
Kevin Lai <kevinlai76@gmail.com>
2019-12-17 14:24:37 -0500

    added a click event on Li with delegation, also added a close button on li

--- main.js
+++ main.js
@@ -1,19 +1,29 @@
-let body = document.querySelector('body')
+// Find all elements
 let form = document.querySelector('form')
 let input = document.querySelector('input')
+let ul = document.querySelector('.content')
 
+// Added a 'submit' event listener to form that points to createLi() passing in the event so that the fn() has access to the data
 form.addEventListener('submit', (event) => {
         event.preventDefault()
         createLi(event)
         event.target.firstElementChild.value = ''
 })
-
+// add or removes 'checked' class to 'Li'
+ul.addEventListener('click', (event)=> {
+        if(event.target.tagName === 'LI'){
+                !!event.target.className ? event.target.className = '' : event.target.className = 'checked'
+                console.log(event.target.className)
+        } 
+})
+// creates Li, with button and event listener on button
 function createLi(event){
         let ul = document.querySelector('.content')
         let li = document.createElement('li')
         let inputValue = event.target.firstElementChild.value
-        li.innerHTML= `
-                <p>${inputValue}<p>
+        li.innerHTML = `
+                ${inputValue}
+                <button type='button'>x</button>
         `
         ul.append(li)
         console.log(ul)
